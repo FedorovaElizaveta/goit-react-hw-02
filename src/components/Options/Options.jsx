c1d4eb6e3d9bd862dbbbb7d964c6ce4e4910313f
FedorovaElizaveta <147994204+FedorovaElizaveta@users.noreply.github.com>
2024-06-07 04:55:09 +0300

    Added styles

--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -1,19 +1,26 @@
+import css from "./Options.module.css";
+
 const Options = ({ feedbackTypes, update, reset, totalFeedback }) => {
   return (
-    <>
+    <div className={css.optionButtonWrapper}>
       {Object.keys(feedbackTypes).map((key) => (
-        <button onClick={() => update(key)} type="button" key={key}>
-          {key}
+        <button
+          onClick={() => update(key)}
+          className={css.optionButton}
+          type="button"
+          key={key}
+        >
+          {key.charAt(0).toUpperCase() + key.slice(1)}
         </button>
       ))}
       {totalFeedback === 0 ? (
         false
       ) : (
-        <button onClick={reset} type="button">
+        <button onClick={reset} className={css.optionButton} type="button">
           Reset
         </button>
       )}
-    </>
+    </div>
   );
 };
 
